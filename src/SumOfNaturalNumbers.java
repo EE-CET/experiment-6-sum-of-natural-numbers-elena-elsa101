0428f24c0cc9fda76c0a678bdb275d427d3f2313
elena-elsa101 <elenaelsacherian@gmail.com>
2026-03-25 20:36:21 +0530

    Update SumOfNaturalNumbers.java

--- src/SumOfNaturalNumbers.java
+++ src/SumOfNaturalNumbers.java
@@ -1,12 +1,20 @@
+  import java.util.Scanner;
 public class SumOfNaturalNumbers {
 
-    // TODO: Create the method public static int NumberSum(int n)
-    // It should calculate and return the sum of first n natural numbers
+ 
 
+    public static int NumberSum(int N) {
+        int sum = 0;
+        for (int i = 1; i <= N; i++) {
+            sum += i;
+        }
+        return sum;
+    }
 
-        // Inside main method
-        // TODO: Read N using Scanner
-        // TODO: Call NumberSum(N)
-        // TODO: Print the result
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(NumberSum(N));
+    }
     
 }
